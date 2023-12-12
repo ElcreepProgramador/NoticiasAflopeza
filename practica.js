61f250a5f4e5ea3cef7aef354a82968ecf866ca4
ElcreepProgramador <143519944+ElcreepProgramador@users.noreply.github.com>
2023-12-12 01:59:14 -0500

    Add files via upload

---
+++ practica.js
@@ -0,0 +1,24 @@
+//var nombre = "La gata";
+//var  Edad = 14;
+//var peso = 145;
+//var apellido =" Santos";
+//alert("El nombvre de la gata es " + nombre);
+//console.log("La cantidad que come la gata es su peso :" +  peso + "" + typeof + peso);
+//console.log("La edad de la gata es :" +  Edad + "" + typeof +  Edad)
+ //var primerNumero = 15;
+ //var segundoNumero = 17;
+ //var resultado  = primerNumero * segundoNumero ;
+ //console.log("La repuesta de la multiplicaion es :" + resultado + "" + typeof + resultado) 
+  //var primernumero = 14 
+//Var 
+// ejercicio  programa de restaurante
+var Co1 = 2800;
+var Co2 = 1300;
+var cuentaDecomida = Co1 + Co2 ;
+alert("la cuenta de la comida es :"+ cuentaDecomida);
+var be1 = 900;
+var be2 = 650;
+var cuentaDeBe = be1 + be2 ;
+alert ("la cuenta de la bebida es :"+ cuentaDeBe);
+var cuentaFinal = cuentaDecomida + cuentaDeBe ;
+alert ("la cuenta final es :"+ cuentaFinal);
